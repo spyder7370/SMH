388152258a9c1e2398308130899cfbc074768edf
Hitesh Yadav <53417879+spyder7370@users.noreply.github.com>
2025-01-31 22:48:46 +0530

    add translation (#2)
    * add translation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import './translations/i18n';
 
 const App = () => {
 	return (
